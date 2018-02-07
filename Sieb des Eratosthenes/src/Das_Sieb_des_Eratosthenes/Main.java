da052cff01d2b1d28ff6707a39dba6ce4963d6f7
BenWyss <BenWyss@github.com>
2018-02-07 15:52:21 +0100

    Eratosthenes
    Finished

---
+++ Sieb des Eratosthenes/src/Das_Sieb_des_Eratosthenes/Main.java
@@ -0,0 +1,7 @@
+package Das_Sieb_des_Eratosthenes;
+
+public class Main {
+    public static void main(String[] args) {
+        Primenumbers.printPrimenumbers();
+    }
+}
---
+++ Sieb des Eratosthenes/src/Das_Sieb_des_Eratosthenes/Primenumbers.java
@@ -0,0 +1,42 @@
+package Das_Sieb_des_Eratosthenes;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Primenumbers {
+
+    public static void printPrimenumbers() {
+        //Zunächst werden alle Zahlen 2, 3, 4,… bis zu einem frei wählbaren Maximalwert aufgeschrieben
+        int maximalWert = 100;
+        
+        // Legt eine neue Liste an
+        ArrayList<Boolean> zahlenListe = new ArrayList<Boolean>();
+
+        // Füllt die neue Liste mit lauter true-Elementen
+        for (int i = 0; i <= maximalWert; i++) {
+            zahlenListe.add(i, true);
+        }
+
+        for (int i = 2; i <= maximalWert; i++) {
+
+            if (zahlenListe.get(i)) {
+                int j = i;
+                do {
+                    j = j + i;
+                    if (j <= maximalWert) {
+                        // Ist die Zahl ein Vielfaches einer 
+                        // Primzahl, dann wird sie mit false 
+                        // markiert
+                        zahlenListe.set(j, false);
+                    }
+                } while (j <= maximalWert);
+            }
+        }
+
+        for (int i = 0; i <= maximalWert - 1; i++) {
+            if (zahlenListe.get(i) && i > 1) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
