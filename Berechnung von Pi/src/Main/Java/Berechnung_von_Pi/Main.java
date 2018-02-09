698394428e11a9b214aa8b6c753f4b6417248976
BenWyss <BenWyss@github.com>
2018-02-09 13:52:30 +0100

    Fixed Bug

--- Berechnung von Pi/src/Main/Java/Berechnung_von_Pi/Main.java
+++ Berechnung von Pi/src/Main/Java/Berechnung_von_Pi/Main.java
@@ -2,6 +2,6 @@ package Berechnung_von_Pi;
 
 public class Main {
     public static void main(String[] args) {
-        Berechnung_von_Pi.Pi.calculatePi(1000000);
+        Pi.calculatePi(1000000);
     }
 }
