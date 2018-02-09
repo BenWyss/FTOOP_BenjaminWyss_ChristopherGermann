695dce0a8faf002cd7d50cf6dfc51e1a694ac452
BenWyss <BenWyss@github.com>
2018-02-09 10:05:01 +0100

    Monte-Carlo-Pi Finished
    Log:
    - Created method
    - Program is technically finished
    
    TODO:
    - Unit Test 5% abweichung!

--- Berechnung von Pi/src/Berechnung_von_Pi/Main.java
+++ Berechnung von Pi/src/Berechnung_von_Pi/Main.java
@@ -2,6 +2,6 @@ package Berechnung_von_Pi;
 
 public class Main {
     public static void main(String[] args) {
-
+        Pi.calculatePi(1000000);
     }
 }
--- Berechnung von Pi/src/Berechnung_von_Pi/Pi.java
+++ Berechnung von Pi/src/Berechnung_von_Pi/Pi.java
@@ -2,7 +2,21 @@ package Berechnung_von_Pi;
 
 public class Pi {
 
-    public static void calculatePi() {
+    public static void calculatePi(int numberOfPoints) {
 
+        int pointsInsideTheCircle = 0;
+        double xCoordinate, yCoordinate;
+
+        for (int i = 1; i <= numberOfPoints; i++) {
+            xCoordinate = Math.random();
+            yCoordinate = Math.random();
+
+            if (Math.hypot(xCoordinate, yCoordinate) <= 1) {
+                pointsInsideTheCircle += 1;
+            }
+        }
+        double monteCarlosPi = 4 * (double) pointsInsideTheCircle / numberOfPoints;
+
+        System.out.printf("%d Tropfen, davon %d Tropfen im Viertelkreis, dementsprechend ist Pi etwa %g%n",numberOfPoints,pointsInsideTheCircle,monteCarlosPi);
     }
 }
