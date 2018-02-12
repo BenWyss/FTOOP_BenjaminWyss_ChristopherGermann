e8e37e22af6bd6c04b6a84346e12df1def1475af
BenWyss <benjamin.wyss@students.ffhs.ch>
2018-02-12 09:59:24 +0100

    Unit Test

--- Berechnung von Pi/src/Unit Test/Java/Berechnung_von_Pi/PiTest.java
+++ Berechnung von Pi/src/Unit Test/Java/Berechnung_von_Pi/PiTest.java
@@ -3,12 +3,10 @@ package Berechnung_von_Pi;
 import junit.framework.TestCase;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
-public class PiTest extends TestCase {
-
+class PiTest extends TestCase {
     @Test
-    public void testCalculatePi() throws Exception {
-        //assertEquals();
+    void calculatePi() {
+        //FUCKING WITZ
     }
+
 }
