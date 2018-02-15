37fce98d505083815ed76099dd3d5762e935ddc5
BenWyss <BenWyss@github.com>
2018-02-15 08:51:20 +0100

    JavaDoc Sieb des Eratosthenes
    Log:
    - Added some JavaDoc, can you check if that's correct?

--- Sieb des Eratosthenes/src/Main/Java/Das_Sieb_des_Eratosthenes/Primenumbers.java
+++ Sieb des Eratosthenes/src/Main/Java/Das_Sieb_des_Eratosthenes/Primenumbers.java
@@ -2,11 +2,31 @@ package Das_Sieb_des_Eratosthenes;
 
 import java.util.ArrayList;
 
+/**
+ * Das Sieb des Eratosthenes
+ *
+ * Dies ist die Hauptklasse, welche die Methode "printPrimenumbers" enthält, welche die Primzahlen widergibt.
+ *
+ * @author Benjamin Wyss & Christopher Germann
+ * @version 1.00
+ **/
+
 public class Primenumbers {
+
+    /**
+     * Introdcution
+     *
+     * <dl>
+     * <dt><span class="strong"> Methode printPrimenumbers </span></dt>
+     * <dd>Diese Methode berechnet alle Primzahlen zwischen 2 und 100 und returniert diese als ArrayList</dd>
+     * </dl>
+     * @return returniert eine ArrayList mit allen Primzahlen von 2 bis 100
+     */
 
     public static ArrayList<Integer> printPrimenumbers() {
         //Zunächst werden alle Zahlen 2, 3, 4,… bis zu einem frei wählbaren Maximalwert aufgeschrieben
         int maximalWert = 100;
+
         // Legt eine neue Liste an
         ArrayList<Boolean> zahlenListe = new ArrayList<Boolean>();
         ArrayList<Integer> numberList = new ArrayList<>();
@@ -21,7 +41,7 @@ public class Primenumbers {
             if (zahlenListe.get(i)) {
                 int j = i;
                 do {
-                    j = j + i;
+                    j += i;
                     if (j <= maximalWert) {
                         // Ist die Zahl ein Vielfaches einer 
                         // Primzahl, dann wird sie mit false 
@@ -32,6 +52,7 @@ public class Primenumbers {
             }
         }
 
+        //Lies alle Primzahlen des arrays aus
         for (int i = 0; i <= maximalWert - 1; i++) {
             if (zahlenListe.get(i) && i > 1) {
                 numberList.add(i);
