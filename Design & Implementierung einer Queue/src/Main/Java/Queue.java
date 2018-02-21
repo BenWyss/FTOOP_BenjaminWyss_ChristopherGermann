64e50bda6d47610388c6be2515438b8fe0d8872d
Benjamin Wyss <benjamin.wyss@schindler.com>
2018-02-21 16:13:13 +0100

    Added JavaDoc (missing Methods)
    Log:
    - Added JavaDoc for Classes
    
    TODO:
    - Add JavaDoc for Interface Methods

--- Design & Implementierung einer Queue/src/Main/Java/Queue.java
+++ Design & Implementierung einer Queue/src/Main/Java/Queue.java
@@ -1,3 +1,12 @@
+/**
+ * Queue
+ *
+ * Dies ist ein Interface, welches die Methoden 'addElement', 'removeElement' und 'isEmpty' enthält.
+ *
+ * @author Benjamin Wyss & Christopher Germann
+ * @version 1.00
+ **/
+
 public interface Queue {
 
     public void addElement(int element);
--- Design & Implementierung einer Queue/src/Main/Java/Stack.java
+++ Design & Implementierung einer Queue/src/Main/Java/Stack.java
@@ -1,11 +1,30 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
+/**
+ * Stack
+ *
+ * Dies ist die Hauptklasse, welche das Interface 'Queue' importiert und deren Methoden spezifiziert.
+ *
+ * @author Benjamin Wyss & Christopher Germann
+ * @version 1.00
+ * @see Queue
+ **/
+
 public class Stack implements Queue {
 
     private int stackSize = 10;
     public int[] stack = new int[stackSize];
     private int nextIndex = 0;
+
+    /**
+     * Methode addElement
+     *
+     * DESCRIPTION
+     *
+     * @param element = Ein Zahlenelement, welches auf den Stack gelegt werden soll.
+     * @return void
+     */
 
     @Override
     public void addElement(int element) {
@@ -19,10 +38,26 @@ public class Stack implements Queue {
         nextIndex++;
     }
 
+    /**
+     * Methode removeElement
+     *
+     * DESCRIPTION
+     *
+     * @return void
+     */
+
     @Override
     public void removeElement() {
         //TODO
     }
+
+    /**
+     * Methode isEmpty
+     *
+     * DESCRIPTION
+     *
+     * @return Gibt zurück, ob der Stack leer ist (true or false -> Boolean).
+     */
 
     @Override
     public boolean isEmpty() {
